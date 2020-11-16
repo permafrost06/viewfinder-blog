76cc25953ae761b005a6a7013e95f463d0246af4
Zahin Azmayeen <zahin06@gmail.com>
2020-11-17 00:05:13 +0600

    changed meta

--- pages/black-and-white-photography-and-subsequent.vue
+++ pages/black-and-white-photography-and-subsequent.vue
@@ -13,7 +13,7 @@ export default Vue.extend({
       title: "সাদাকালো ছবি ও তৎ সম্পর্কিত অন্যান্য বিষয়াবলী - Viewfinder | SUPA",
       meta: [
         { hid: 'description', name: 'description', content: 'প্রথম ক্যামেরা আবিষ্কার হয় ১৮২০ এর দশকে। সেই সময়ের আগে মানুষ ছবি তোলার জন্য প্রচলিত মিডিয়াতগুলোর উপর নির্ভরশীল ছিল যেমন- চিত্রকর্ম, স্কেচ এবং অঙ্কন। যাইহোক, যখন ক্যামেরায় ছবি তোলা হয়েছিল তখন মনে হয়েছিল ঐতিহ্যবাহী মিডিয়াগুলির চেয়ে ক্যামেরার ছবি কোন বস্তু সম্পর্কে আরও তথ্যবহুল বা বিশদভাবে উপস্থাপন করেছিল।' },
-        { hid: 'og:image', property: 'og:image', content: '/article-4.jpg' },
+        { hid: 'og:image', property: 'og:image', content: 'https://viewfinder.supasust.org/article-4.jpg' },
         { hid: 'og:description', property: 'og:description', content: 'প্রথম ক্যামেরা আবিষ্কার হয় ১৮২০ এর দশকে। সেই সময়ের আগে মানুষ ছবি তোলার জন্য প্রচলিত মিডিয়াতগুলোর উপর নির্ভরশীল ছিল যেমন- চিত্রকর্ম, স্কেচ এবং অঙ্কন। যাইহোক, যখন ক্যামেরায় ছবি তোলা হয়েছিল তখন মনে হয়েছিল ঐতিহ্যবাহী মিডিয়াগুলির চেয়ে ক্যামেরার ছবি কোন বস্তু সম্পর্কে আরও তথ্যবহুল বা বিশদভাবে উপস্থাপন করেছিল।' },
         { hid: 'og:url', property: 'og:url', content: 'https://viewfinder.supasust.org/black-and-white-photography-and-subsequent/' },
         { hid: 'og:title', property: 'og:title', content: "সাদাকালো ছবি ও তৎ সম্পর্কিত অন্যান্য বিষয়াবলী - Viewfinder | SUPA" }
