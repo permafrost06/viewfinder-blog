e624e77a718f9751899a41b6c41cadd4436da68d
Zahin Azmayeen <zahin06@gmail.com>
2020-11-17 00:44:24 +0600

    renamed file -_-

--- pages/street-photography-using-mobile-phone-camera.vue
+++ pages/street-photography-using-mobile-phone-camera.vue
@@ -13,7 +13,7 @@ export default Vue.extend({
       title: "Street Photography using Mobile Phone Camera - Viewfinder | SUPA",
       meta: [
         { hid: 'description', name: 'description', content: 'Photography is an art of vision that can be expressed through a bunch of pixels and hence should not always depend on high budget devices. Nowadays Smartphone has replaced cameras and other devices in many aspects of photography. It is because budget phones have decent cameras and can get you some amazing results. Though the camera and lens in your Smartphone isn’t as good as a professional digital camera, but in street photography, using a phone sometimes can be of advantage.' },
-        { hid: 'og:image', property: 'og:image', content: '/article-1.jpg' },
+        { hid: 'og:image', property: 'og:image', content: '/article-2.jpg' },
         { hid: 'og:description', property: 'og:description', content: 'Photography is an art of vision that can be expressed through a bunch of pixels and hence should not always depend on high budget devices. Nowadays Smartphone has replaced cameras and other devices in many aspects of photography. It is because budget phones have decent cameras and can get you some amazing results. Though the camera and lens in your Smartphone isn’t as good as a professional digital camera, but in street photography, using a phone sometimes can be of advantage.' },
         { hid: 'og:url', property: 'og:url', content: 'https://viewfinder.supasust.org/street-photography-using-mobile-phone-camera/' },
         { hid: 'og:title', property: 'og:title', content: "Street Photography using Mobile Phone Camera - Viewfinder | SUPA" }
